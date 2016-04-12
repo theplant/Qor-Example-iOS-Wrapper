a89243260fdd462e42d03fa399724eec67904332
nice-chow <602040511@qq.com>
2016-04-12 20:54:30 +0800

    add web wrapper for qor example web app

--- src/home/RootViewController.swift
+++ src/home/RootViewController.swift
@@ -7,17 +7,52 @@
 //
 
 import UIKit
+import Cartography
 
-class RootViewController: UIViewController {
+class RootViewController: UIViewController, UIWebViewDelegate {
 
+    var webView: UIWebView = UIWebView(frame: CGRectZero)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "Root"    
-        view.backgroundColor = UIColor.cyanColor()    
+        title = "Root"
+        edgesForExtendedLayout = .None
+        view.backgroundColor = UIColor.cyanColor()
+        navigationController!.navigationBarHidden = true
+        
+        view.addSubview(webView)
+        webView.delegate = self
+        constrain(webView) { (w) in
+            w.top == w.superview!.top
+            w.bottom == w.superview!.bottom
+            w.leading == w.superview!.leading
+            w.trailing == w.superview!.trailing
+        }
+        
+        let req = NSURLRequest(URL:
+            NSURL(string: "/admin",
+            relativeToURL:NSURL(string: "http://localhost:7000")!
+            )!)
+        webView.loadRequest(req)
+        
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
+    }
+    
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        //
+        
+        return true
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+        //
+    }
+    
+    func webView(webView: UIWebView, didFailLoadWithError error: NSError?) {
+        //
     }
 }
